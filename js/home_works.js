145fd8c9eee724c5777d570d9bbd4f74eda057f2
pyMK-24 <karimmohammad1406@gmail.com>
2025-07-08 15:41:17 +0600

    hw_2

--- js/home_works.js
+++ js/home_works.js
@@ -12,16 +12,61 @@ buttonGmail.onclick = () => {
         spanGmail.innerHTML = "Неверно, попробуй еще раз."
     }
 }
-//hw 1.2
+//hw 1.2 - 2.2
 const childBlock = document.querySelector(".child_block");
+const parrentBlock = document.querySelector(".parent_block");
 
-let position = 0;
+const offsetWidth = parrentBlock.clientWidth - childBlock.clientWidth;
+const offsetHeight = parrentBlock.clientHeight - childBlock.clientHeight;
+
+let positionX = 0;
+let positionY = 0;
 
 const actionBlock = () => {
-    position++
-    childBlock.style.left = position + "px"
-    if (position <= 448) {
-        requestAnimationFrame(actionBlock);
+    if (positionX <= offsetWidth && positionY === 0) {
+        childBlock.style.left = positionX + "px";
+        positionX++;
+    } else if (positionX >= offsetWidth && positionY <= offsetHeight) {
+        childBlock.style.top = positionY + "px";
+        positionY++;
+    } else if (positionX > 0 && positionY >= offsetHeight) {
+        childBlock.style.left = positionX + "px"; 
+        positionX--;       
+    } else if (positionX === 0 && positionY > 0) {
+        childBlock.style.top = positionY + "px";
+        positionY--;
     }
-}
+    requestAnimationFrame(actionBlock);
+};
+
 requestAnimationFrame(actionBlock);
+
+//hw 2.2
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
+const resetButton = document.querySelector("#reset");
+const interval = document.querySelector(".interval")
+
+let timer = null;
+let count = 0;
+
+startButton.onclick = () => {
+    if (timer === null) {
+        timer = setInterval(() => {
+            count++;
+            interval.textContent = count;
+        },1000)
+    }
+};
+
+stopButton.onclick = () => {
+    clearInterval(timer)
+    timer = null;
+};
+
+resetButton.onclick = () => {
+    clearInterval(timer)
+    timer = null;
+    count = 0;
+    interval.textContent = count;
+};
