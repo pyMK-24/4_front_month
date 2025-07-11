fc5feb1a57166707c7cc5449d2923cf3e287dd51
pyMK-24 <karimmohammad1406@gmail.com>
2025-07-11 17:58:12 +0600

    hw_3

--- js/lesson.js
+++ js/lesson.js
@@ -13,3 +13,54 @@ phoneButton.onclick = () => {
         phoneResult.innerHTML = "notok"
     }
 }
+
+//TAB SLIDER
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block');
+const tabs = document.querySelectorAll(".tab_content_item");
+const tabParrent = document.querySelector(".tab_content_items");
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach((item) => {
+        item.style.display = 'none';
+    })
+    tabs.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+};
+
+const showTabContent = (i = 0) => {
+    tabContentBlocks[i].style.display = 'block';
+    tabs[i].classList.add("tab_content_item_active");
+};
+
+
+hideTabContent();
+showTabContent()
+
+let indexSlider = 0;
+
+tabParrent.onclick = (event) => {
+    if (event.target.classList.contains("tab_content_item")) {
+        tabs.forEach((tab, index) => {
+            if (event.target === tab) {
+                hideTabContent();
+                showTabContent(index);
+                indexSlider = index;
+            }
+        })
+    }
+};
+
+const autoSliderTabs = () => {
+    setInterval(() => {        
+        indexSlider++
+        if (indexSlider > tabs.length - 1) {
+            indexSlider = 0;
+        }
+        hideTabContent()
+        showTabContent(indexSlider)}
+        ,3000)
+}
+
+autoSliderTabs();
